48fc8b9f9977bc743bccaea25d036a7c85751531
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-19 12:14:58 -0400

    chamfer the head

--- chamferedScrew.groovy
+++ chamferedScrew.groovy
@@ -23,7 +23,7 @@ import com.google.gson.reflect.TypeToken;
 	
 import eu.mihosoft.vrl.v3d.parametrics.*;
 CSG getNut(){
-	String type= "capScrew"
+	String type= "chamferedScrew"
 	LengthParameter facets		= new LengthParameter("Bolt Hole Facet Count",10,[40,10])
 	LengthParameter boltLength		= new LengthParameter("Bolt Length",10,[180,10])
 	LengthParameter offset		= new LengthParameter("printerOffset",0.0,[2,0])
@@ -44,7 +44,7 @@ CSG getNut(){
 		boltLength.setMM(servoConfig.get("boltLength"))
 
 	
-	CSG head =new Cylinder(headDiameter/2,headDiameter/2,headHeight,(int)facets.getMM()).toCSG() // a one line Cylinder
+	CSG head =new Cylinder(outerDiameter/2,headDiameter/2,headHeight,(int)facets.getMM()).toCSG() // a one line Cylinder
 				.toZMin()
 	CSG shaft =new Cylinder(outerDiameter/2,outerDiameter/2,boltLength.getMM(),(int)facets.getMM()).toCSG() // a one line Cylinder
 				.toZMax()
